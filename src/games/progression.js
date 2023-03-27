940540423dd2ac381da2b1afed04f2065544d996
AnnaHex <A.A.Beletskaja@gmail.com>
2023-03-27 18:15:30 +0300

    add changes

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,14 +3,14 @@ import showName from '../index.js';
 
 const userName = showName();
 const length = 10;
-const getRandomInt = (max) => Math.floor(Math.random() * max);
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 const progression = (questionIndex, start, diff) => {
   let question = '';
   let rightAnswer = '';
   for (let i = 0; i < length; i += 1) {
     const nextElem = (i === questionIndex) ? '..' : start + (diff * i);
-    question += `${nextElem}`;
+    question += ` ${nextElem}`;
   }
   rightAnswer = start + questionIndex * diff;
   return [question, rightAnswer];
@@ -19,9 +19,9 @@ const progression = (questionIndex, start, diff) => {
 const progressionGame = () => {
   console.log('What number is missing in the progression?');
   for (let i = 0; i < 3; i += 1) {
-    const questionIndex = getRandomInt(length - 1);
-    const start = getRandomInt(10);
-    const diff = getRandomInt(5);
+    const questionIndex = getRandomInt(1, length - 1);
+    const start = getRandomInt(1, 10);
+    const diff = getRandomInt(1, 5);
     const values = progression(questionIndex, start, diff);
     console.log('Question:', values[0]);
     const answerUser = readlineSync.question('Your answer:');
