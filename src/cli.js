0262e8cb915ce19b4cd9fd458c7806397b61e3ff
AnnaHex <A.A.Beletskaja@gmail.com>
2023-09-06 13:36:48 +0300

    add

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const showGreeting = () => {
+  const userName = readlineSync.question('May I have your name?');
+  const greeting = 'Hello';
+  console.log(`${greeting}, ${userName}!`);
+};
+export default showGreeting;
