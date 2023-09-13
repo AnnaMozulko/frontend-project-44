ee1e225e43732b88cc5f9a336fb83b26c250e2ab
AnnaHex <A.A.Beletskaja@gmail.com>
2023-09-13 20:16:31 +0300

    add

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,19 +6,15 @@ const description = 'What is the result of the expression?';
 
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
-
 const findCalcPair = () => {
-
-  for (let i = 0; i < 3; i +=1) {
+  for (let i = 0; i < 3; i += 1) {
     const numOne = generateRandomNum(3, 10);
     const numTwo = generateRandomNum(1, 2);
     const signs = ['+', '-', '*'];
     const sign = signs[getRandomInt(signs.length)];
 
     const verity = () => {
-
       let rightAnswer = 0;
-    
     
     switch (sign) {
       case '+':
@@ -33,16 +29,11 @@ const findCalcPair = () => {
         default:
           rightAnswer = null;
     }
-      
        return rightAnswer; 
-      
     };
 
-  
-  
   const question = `${numOne} ${sign} ${numTwo}`;
   const rightAnswer = verity();
-  
   
   return cons(question, String(rightAnswer));
   }
