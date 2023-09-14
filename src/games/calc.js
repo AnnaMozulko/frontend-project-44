a4f6db0ce392e7ae5307920588478f27e1263e86
AnnaHex <A.A.Beletskaja@gmail.com>
2023-09-14 12:14:18 +0300

    add

--- src/games/calc.js
+++ src/games/calc.js
@@ -7,31 +7,31 @@ const description = 'What is the result of the expression?';
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const operation = () => {
-const signs = ['+', '-', '*'];
-const sign = signs[getRandomInt(signs.length)]; 
-return sign;
+  const signs = ['+', '-', '*'];
+  const sign = signs[getRandomInt(signs.length)];
+  return sign;
 };
 
 const findCalcPair = () => {
-      const numOne = generateRandomNum(3, 10);
-      const numTwo = generateRandomNum(1, 2);
-      const sign = operation();
-      let rightAnswer;
-      let question;
-      switch (sign) {
-        case '0':
-          question = `${numOne} + ${numTwo}`;
-          rightAnswer = numOne + numTwo;
-          break;
-        case '1':
-          question = `${numOne} - ${numTwo}`;
-          rightAnswer = numOne - numTwo;
-          break;
-        default:
-          question = `${numOne} * ${numTwo}`;
-          rightAnswer = numOne * numTwo;
-      }
-      return cons(question, String(rightAnswer));
-    };
+  const numOne = generateRandomNum(3, 10);
+  const numTwo = generateRandomNum(1, 2);
+  const sign = operation();
+  let rightAnswer;
+  let question;
+  switch (sign) {
+    case '0':
+      question = `${numOne} + ${numTwo}`;
+      rightAnswer = numOne + numTwo;
+      break;
+    case '1':
+      question = `${numOne} - ${numTwo}`;
+      rightAnswer = numOne - numTwo;
+      break;
+    default:
+      question = `${numOne} * ${numTwo}`;
+      rightAnswer = numOne * numTwo;
+  }
+  return cons(question, String(rightAnswer));
+};
 
 export default () => startGame(description, findCalcPair);
